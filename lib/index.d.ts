04503ebb175dd8b4c68a266ff0b4f2dceb27d258
微若蜉蝣 <2383155523@qq.com>
2022-10-06 10:54:32 +0800

    release: 发布正式版1.0.0

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Vnode {
-  type: string
-  tag: string | null
-  props: object | null
-  children: Array<Vnode> | string
-}
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script lang="ts" setup>
 import { shallowRef, onMounted, h } from "vue"
-import { templateTransformNodes, nodesTransformVNodes } from "../lib/index"
-import type { Vnode } from "../lib/index.d"
-// <div id="root"><span><i><b>11</b></i></span></div>
+import { templateTransformNodes, nodesTransformVNodes } from "xiao-you-transform-v3"
+
 const template = `<div class="red">111</div><div>111</div>`
 const root = shallowRef<HTMLElement>()
 const node = templateTransformNodes(template) //dom节点
 console.log("node=", node)
 console.log(nodesTransformVNodes(node, {}))
-
-// console.log(nodesTransformVNodes(templateTransformNodes(template)))
-// onMounted(() => {
-//   console.dir(root.value)
-// })
 </script>
 
 <template>
